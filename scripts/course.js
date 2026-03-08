8a34f2d400c11d7bd905ef85d9725f72b69742a1
barklin-01 <barklin2003@gmail.com>
2026-03-07 19:30:46 -0500

    new

--- scripts/course.js
+++ scripts/course.js
@@ -1,79 +1,79 @@
-cursos constantes = [
+const courses = [
     {
-        Asunto: 'CSE',
-        número: 110,
-        Título: 'Introducción a la programación',
-        créditos: 2,
-        Certificado: 'Programación Web y de Computadoras',
-        Descripción: Este curso introducirá a los estudiantes a la programación.Presentará los fundamentos de los lenguajes de programación(variables, decisiones, cálculos, bucles, matrices y entrada/ salida) y los utilizará para resolver problemas.
-        tecnología: [
-        'Pitón'
-    ],
-    completado: falso
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: false
     },
     {
-        Asunto: 'WDD',
-        número: 130,
-        Título: 'Fundamentos de la Web',
-        créditos: 2,
-        Certificado: 'Programación Web y de Computadoras',
-        Descripción: Este curso introduce a los estudiantes a la World Wide Web y a las carreras profesionales en diseño y desarrollo web.El curso es práctico, con la participación de los estudiantes en diseños y programación web sencillos.Se espera que los estudiantes que completen este curso comprendan los campos del diseño y desarrollo web y tengan una buena idea de si desean cursar esta carrera como especialización.
-            tecnología: [
-                'HTML',
-                'CSS'
-            ],
-        completado: falso
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: false
     },
     {
-        Asunto: 'CSE',
-        número: 111,
-        Título: 'Programación con funciones',
-        créditos: 2,
-        Certificado: 'Programación Web y de Computadoras',
-        Descripción: Los estudiantes de CSE 111 se convierten en programadores informáticos más organizados, eficientes y competentes al aprender a investigar y llamar a funciones escritas por otros; a escribir, llamar, depurar y probar sus propias funciones; y a gestionar errores dentro de las funciones.Los estudiantes de CSE 111 escriben programas con funciones para resolver problemas en diversas disciplinas, como negocios, ciencias físicas, desempeño humano y humanidades.
-            tecnología: [
-                'Pitón'
-            ],
-        completado: falso
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: false
     },
     {
-        Asunto: 'CSE',
-        número: 210,
-        Título: 'Programación con clases',
-        créditos: 2,
-        Certificado: 'Programación Web y de Computadoras',
-        Descripción: Este curso presentará el concepto de clases y objetos.Presentará la encapsulación a nivel conceptual.También trabajará con la herencia y el polimorfismo.
-            tecnología: [
-                'DO#'
-            ],
-        completado: falso
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: false
     },
     {
-        Asunto: 'WDD',
-        número: 131,
-        Título: 'Fundamentos de la Web dinámica',
-        créditos: 2,
-        Certificado: 'Programación Web y de Computadoras',
-        Descripción: Este curso se basa en la experiencia previa en Fundamentos Web y programación.Los estudiantes aprenderán a crear sitios web dinámicos que usan JavaScript para responder a eventos, actualizar contenido y crear experiencias de usuario responsivas.
-            tecnología: [
-                'HTML',
-                'CSS',
-                'JavaScript'
-            ],
-        completado: falso
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
     },
     {
-        Asunto: 'WDD',
-        número: 231,
-        Título: 'Desarrollo Web Frontend I',
-        créditos: 2,
-        Certificado: 'Programación Web y de Computadoras',
-        Descripción: Este curso se basa en la experiencia previa con Fundamentos de Web Dinámica y programación.Los estudiantes se centrarán en la experiencia del usuario, la accesibilidad, el cumplimiento normativo, la optimización del rendimiento y el uso básico de API.
-            tecnología: [
-                'HTML',
-                'CSS',
-                'JavaScript'
-            ],
-        completado: falso
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
     }
 ]
